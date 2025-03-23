82427d49d109d2dcfb5390decda0ad7f7c7f0462
klapeto <yiannispana@hotmail.com>
2025-03-23 16:11:17 +0200

    Radiance Changes

--- src/GitcSimulator.WishSimulator/Program.cs
+++ src/GitcSimulator.WishSimulator/Program.cs
@@ -2,7 +2,34 @@
 {
 	public class Program
 	{
+		private static readonly Random Random = new();
+
 		public static void Main(string[] args)
+		{
+			SimulateStatistics();
+			//SimulateHowManyWishesItTakes();
+		}
+
+		private static void SimulateHowManyWishesItTakes()
+		{
+			var result = PerformSimulation(
+				c => c.Cons5StarsPromo < 6
+			);
+			Console.WriteLine($"Took {result.Wishes} to get the desired. In the mean time you got:");
+			Console.WriteLine($" - C{result.Cons5StarsPromo} 5Star promo");
+			Console.WriteLine($" - C{result.Cons5StarsStandard} 5Star Standards");
+			Console.WriteLine($" - C{result.Cons4Stars[0]} 4Star promo 1");
+			Console.WriteLine($" - C{result.Cons4Stars[1]} 4Star promo 2");
+			Console.WriteLine($" - C{result.Cons4Stars[2]} 4Star promo 3");
+			Console.WriteLine($" - C{result.Cons4StarsStandard} 4Star standards");
+			Console.WriteLine($"5Star wins: {result.Wins5Stars}");
+			Console.WriteLine($"5Star Loss: {result.Loss5Stars}");
+			Console.WriteLine($"4Star Wins: {result.Wins4Stars}");
+			Console.WriteLine($"4Star Loss: {result.Loss4Stars}");
+			Console.WriteLine($"Captured radiances: {result.RadianceCaptured}");
+		}
+
+		private static void SimulateStatistics()
 		{
 			const int simulationCount = 1000000;
 			var tasks = new List<Task<BatchResult>>();
@@ -40,21 +67,6 @@
 			var min = tasks.Min(t => t.Result.Min);
 
 			Console.WriteLine($"Min: {min}, Max: {max}: Avg: {avg:F2}");
-
-			// var result = PerformSimulation(
-			// 	c => c.Cons4Stars[0] < 6
-			// );
-			// Console.WriteLine($"Took {result.Wishes} to get the desired 4 star promo. In the mean time you got:");
-			// Console.WriteLine($" - C{result.Cons5StarsPromo} 5Star promo");
-			// Console.WriteLine($" - C{result.Cons5StarsStandard} 5Star Standards");
-			// Console.WriteLine($" - C{result.Cons4Stars[0]} 4Star promo 1");
-			// Console.WriteLine($" - C{result.Cons4Stars[1]} 4Star promo 2");
-			// Console.WriteLine($" - C{result.Cons4Stars[2]} 4Star promo 3");
-			// Console.WriteLine($" - C{result.Cons4StarsStandard} 4Star standards");
-			// Console.WriteLine($"5Star wins: {result.Wins5Stars}");
-			// Console.WriteLine($"5Star Loss: {result.Loss5Stars}");
-			// Console.WriteLine($"5Star Wins: {result.Wins4Stars}");
-			// Console.WriteLine($"4Star Loss: {result.Loss4Stars}");
 		}
 
 		private static WishSimulationResult PerformSimulation(
@@ -76,22 +88,18 @@
 			var char5Star = -1;
 			var char5StarStandard = -1;
 
-			var cons = -1;
-
 			var guarantee4Star = false;
 			var guarantee5Star = false;
 
 			var currentBatch4Star = 0;
 			var currentBatch5Star = 0;
 
-			var rnd = new Random();
 			var star5Chance = 0.6;
 			var star5Wins = 0;
 			var star5Loss = 0;
 			var star4Wins = 0;
 			var star4Loss = 0;
 
-			var radiance = 0.0;
 			var radianceCaptured = 0;
 
 			while (keepGoing(
@@ -140,7 +148,7 @@
 					else
 					{
 						var won = Wish(50);
-						var radianceWon = !won && Wish(50 + radiance);
+						var radianceWon = !won && Wish(10);
 						if (won || radianceWon)
 						{
 							if (radianceWon)
@@ -148,7 +156,6 @@
 								radianceCaptured++;
 							}
 
-							radiance = 0.0;
 							char5Star++;
 							guarantee5Star = false;
 							star5Wins++;
@@ -168,7 +175,6 @@
 						}
 						else
 						{
-							radiance += 25.0;
 							char5StarStandard++;
 							guarantee5Star = true;
 							star5Loss++;
@@ -197,7 +203,7 @@
 					if (guarantee4Star)
 					{
 						guarantee4Star = false;
-						var ch = rnd.Next(0, chars4Star.Length);
+						var ch = Random.Next(0, chars4Star.Length);
 						chars4Star[ch]++;
 
 						on4StarGuarantee?.Invoke(
@@ -217,7 +223,7 @@
 					{
 						if (Wish(50))
 						{
-							var ch = rnd.Next(0, chars4Star.Length);
+							var ch = Random.Next(0, chars4Star.Length);
 							chars4Star[ch]++;
 							guarantee4Star = false;
 							star4Wins++;
@@ -275,8 +281,7 @@
 
 		private static bool Wish(double chance)
 		{
-			var rnd = new Random();
-			return rnd.NextDouble() < chance / 100.0;
+			return Random.NextDouble() < chance / 100.0;
 		}
 
 		private class BatchResult
@@ -318,6 +323,7 @@
 				Loss5Stars = loss5Stars;
 				Wins4Stars = wins4Stars;
 				Loss4Stars = loss4Stars;
+				RadianceCaptured = radianceCaptured;
 			}
 
 			public int Wishes { get; }
